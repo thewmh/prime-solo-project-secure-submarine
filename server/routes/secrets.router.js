cb0c949d5910deae0e75b3a35038d76db84ccc5c
William Harrison <wm.harrison@gmail.com>
2018-11-14 17:04:58 -0600

    completed base mode

---
+++ server/routes/secrets.router.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
+
+router.get('/', rejectUnauthenticated, (req, res) => {
+    console.log('req.user:', req.user.clearance_level);
+    clearance_level = req.user.clearance_level;
+    pool.query(`SELECT * FROM "secret" WHERE ${clearance_level} >= secret.secrecy_level`)
+        .then(results => res.send(results.rows))
+        .catch(error => {
+            console.log('Error making SELECT for secrets:', error);
+            res.sendStatus(500);
+        });
+});
+
+module.exports = router;
